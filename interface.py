1bf1821d193db9e4be5e84330d349105e122e991
Nicholas W. Heyer <60236014+nickheyer@users.noreply.github.com>
2021-07-29 03:20:12 -0700

    INSPECT ME

---
+++ interface.py
@@ -0,0 +1,48 @@
+import json
+from PySimpleGUI.PySimpleGUI import Titlebar
+from cryptography.fernet import Fernet
+from time import sleep
+import PySimpleGUI as sg
+import os
+
+
+def pysimp_interface():
+    sg.theme('Reddit')
+    icon_path = os.path.join(os.path.dirname(__file__), 'cc_small_png.png')    
+    layout = [[Titlebar(title = "CyrypticConvert", icon = icon_path, background_color= "dark grey")],
+            [sg.Text("Put your un-encrypted text here: "), sg.Text('Your encryption key is:', pad = ((165, 0), (0,0)))],
+            [sg.Input(key='-IN-', size = ("50",None)), sg.Input(key = "-KEY-", size = ("50", None), readonly=True)],
+            [sg.Button('Encrypt')],
+            [sg.Text("Put your encrypted text here: "), sg.Text('Put your encryption key here: ', pad = ((182, 0), (0,0)))],
+            [sg.Input(key='-EIN-', size = ("50",None)), sg.Input(key = "-EKEY-", size = ("50", None), readonly=False)],
+            [sg.Button('Decrypt'), sg.Text(key = 'KeyCheck', justification = "right", font = ("arial", 8), text_color = "green", s = ("50", None), pad = ((256, 0), (0,0)))]]
+
+    window = sg.Window('CrypticConvert', layout)
+    #event loop for input, makes the window persistent
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == 'Exit':
+            break
+        if event == 'Encrypt':
+            key = Fernet.generate_key()
+            big_key = key
+            window['-KEY-'].update(key)
+            window['-EKEY-'].update(key)
+            window['KeyCheck'].update("Encryption-key copied from above.")
+            fernet = Fernet(key)
+            encoded = json.dumps(values['-IN-']).encode('utf-8')
+            encrypted_bytes = fernet.encrypt(encoded)
+            encrypted = encrypted_bytes.decode('utf-8')
+            sg.clipboard_set(encrypted)
+            sg.PopupNoFrame('See below for encrypted text: \n', encrypted, '(Copied to clipboard)', background_color = "grey")
+        elif event == "Decrypt":
+            key = values['-EKEY-']
+            key = key.strip('b')
+            fernet = Fernet(key)
+            encoded = values["-EIN-"].encode('utf-8')
+            decrypted_bytes = fernet.decrypt(encoded)
+            decrypted = decrypted_bytes.decode('utf-8')
+            sg.clipboard_set(decrypted)
+            sg.PopupNoFrame('See below for decrypted text: \n', decrypted, '(Copied to clipboard)', background_color = "grey")
+    #closes window when breaking out of event loop
+    window.close()
---
+++ main.py
@@ -0,0 +1,7 @@
+from interface import pysimp_interface
+
+def main():
+    pysimp_interface()
+
+if __name__ == "__main__":
+    main()
